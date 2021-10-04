addd355d2d49de8c3ee735ae00d99a2d8172d43b
zhengyao603 <zhengyao@ualberta.ca>
2021-10-04 05:28:35 -0600

    update send post/get request

--- httpclient.py
+++ httpclient.py
@@ -24,21 +24,24 @@ import re
 # you may use urllib to encode data appropriately
 import urllib.parse
 
+
 def help():
     print("httpclient.py [GET/POST] [URL]\n")
+    return
+
 
 class HTTPResponse(object):
     def __init__(self, code=200, body=""):
         self.code = code
         self.body = body
 
+
 class HTTPClient(object):
     #def get_host_port(self,url):
-
     def connect(self, host, port):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect((host, port))
-        return None
+        return
 
     def get_code(self, data):
         return None
@@ -54,6 +57,7 @@ class HTTPClient(object):
         
     def close(self):
         self.socket.close()
+        return
 
     # read everything from the socket
     def recvall(self, sock):
@@ -68,28 +72,54 @@ class HTTPClient(object):
         return buffer.decode('utf-8')
 
     def GET(self, url, args=None):
+        # send GET request and recieve response from server
+        ipaddr = urllib.parse.urlparse(url)[1]
+        path = urllib.parse.urlparse(url)[2]
+        self.connect(ipaddr, 80)
+        self.sendall("GET %s HTTP/1.1\r\nHost: %s\r\nAccept: text/html,text/css\r\nConnection: close\r\n\r\n" % (path, urllib.parse.urlparse(url)[1]))
+        print(self.recvall(self.socket))
+        self.close()
+
         code = 500
         body = ""
         return HTTPResponse(code, body)
 
     def POST(self, url, args=None):
+        # send POST request and recieve response from server
+        ipaddr = urllib.parse.urlparse(url)[1]
+        path = urllib.parse.urlparse(url)[2]
+        self.connect(ipaddr, 80)
+        self.sendall("POST %s HTTP/1.1\r\nHost: %s\r\nContent-type:application/x-www-form-urlencoded\r\nContent-length:%d\r\n\r\n" % (path, urllib.parse.urlparse(url)[1], 0))
+        print(self.recvall(self.socket))
+        self.close()
+
         code = 500
         body = ""
         return HTTPResponse(code, body)
 
+
+    # processing http requests
     def command(self, url, command="GET", args=None):
+        # if it is http POST
         if (command == "POST"):
-            return self.POST( url, args )
+            return self.POST(url, args)
+        # if it is http GET
         else:
-            return self.GET( url, args )
+            return self.GET(url, args)
     
 if __name__ == "__main__":
     client = HTTPClient()
-    command = "GET"
+    # command = "GET"
+
+    # if no argument provided
     if (len(sys.argv) <= 1):
         help()
         sys.exit(1)
+
+    # if http method and url are both provided
     elif (len(sys.argv) == 3):
-        print(client.command( sys.argv[2], sys.argv[1] ))
+        print(client.command(sys.argv[2], sys.argv[1]))
+
+    # if only url provided
     else:
-        print(client.command( sys.argv[1] ))
+        print(client.command(sys.argv[1]))
